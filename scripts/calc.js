1f5c765e59f1340514189cca6413331b602fb0d0
daniepet82 <144553143+daniepet82@users.noreply.github.com>
2023-10-08 20:33:45 +0200

    Update

--- scripts/calc.js
+++ scripts/calc.js
@@ -6,7 +6,7 @@ let operator_2 = null;
 let result = null;
 const buttons = document.querySelectorAll('button');
 const numpad_keys = ['Numpad0','Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9', 'NumpadAdd', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'NumpadDecimal', 'NumpadEnter']
-const data_keys = ['Backspace', 'Slash','Delete', 'Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9']
+const data_keys = ['Enter', 'Backspace', 'Slash','Delete', 'Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9']
 
 window.addEventListener(
     "keydown",
