873dd1608370bfe4eaa490f7fe029c1d9821cc81
daniepet82 <144553143+daniepet82@users.noreply.github.com>
2023-10-08 20:31:43 +0200

    Minor update

--- scripts/calc.js
+++ scripts/calc.js
@@ -5,14 +5,14 @@ let operator_1 = null;
 let operator_2 = null;
 let result = null;
 const buttons = document.querySelectorAll('button');
-const numpad_buttons = ['Numpad0','Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9', 'NumpadAdd', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'NumpadDecimal', 'NumpadEnter']
+const numpad_keys = ['Numpad0','Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9', 'NumpadAdd', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'NumpadDecimal', 'NumpadEnter']
 const data_keys = ['Backspace', 'Slash','Delete', 'Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9']
 
 window.addEventListener(
     "keydown",
     (event) => {
       let key = null
-      if (numpad_buttons.includes(event.code) ){
+      if (numpad_keys.includes(event.code) ){
         key = document.querySelector(`button[numpad-key='${event.code}']`);
       }
       else if (data_keys.includes(event.code) ){
