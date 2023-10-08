30c1b7fa15cd8994724e727c53d5b5c858701b9c
daniepet82 <144553143+daniepet82@users.noreply.github.com>
2023-10-08 20:48:44 +0200

    Minor adjustment

--- scripts/calc.js
+++ scripts/calc.js
@@ -6,11 +6,12 @@ let operator_2 = null;
 let result = null;
 const buttons = document.querySelectorAll('button');
 const numpad_keys = ['Numpad0','Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9', 'NumpadAdd', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'NumpadDecimal', 'NumpadEnter']
-const data_keys = ['Enter', 'Backspace', 'Slash','Delete', 'Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9']
+const data_keys = ['Period', 'Enter', 'Backspace', 'Slash','Delete', 'Digit0','Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9']
 
 window.addEventListener(
     "keydown",
     (event) => {
+      console.log("Event code: "+event.code)
       let key = null
       if (numpad_keys.includes(event.code) ){
         key = document.querySelector(`button[numpad-key='${event.code}']`);
